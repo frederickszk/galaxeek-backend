be08df53b63840939b2c73bc638cdf9a431f842b
frederickszk <45565991+frederickszk@users.noreply.github.com>
2022-01-12 11:47:28 +0800

    Add cross-field support

--- api/views_user.py
+++ api/views_user.py
@@ -18,17 +18,41 @@ def login(request):
         print(login_name, login_password)
         user = User.objects.get(name=login_name)
         if login_password == user.password:
-            return Response({"result": "success", "msg": "Log in successfully.", "user_id": user.id},
+            return Response({"status": "ok", "msg": "Log in successfully.", "user_id": user.id},
                             status=status.HTTP_200_OK)
         else:
-            return Response({"result": "fail", "msg": "Wrong password."},
+            return Response({"status": "error", "msg": "Wrong password."},
                             status=status.HTTP_400_BAD_REQUEST)
     except api.models.User.DoesNotExist:
-        return Response({"result": "fail", "msg": "The user does not exist"},
+        return Response({"status": "error", "msg": "The user does not exist"},
                         status=status.HTTP_400_BAD_REQUEST)
     except KeyError:
-        return Response({"result": "fail", "msg": "Please check the request body format."},
+        return Response({"status": "error", "msg": "Please check the request body format."},
                         status=status.HTTP_400_BAD_REQUEST)
     except Exception:
-        return Response({"result": "fail", "msg": "Unknown exception."},
+        return Response({"status": "error", "msg": "Unknown exception."},
                         status=status.HTTP_400_BAD_REQUEST)
+#
+# @api_view(['POST'])
+# def login(request):
+#     # try:
+#     # print(request.data)
+#     login_name = request.data['user']
+#     login_password = request.data['password']
+#     print(login_name, login_password)
+#     user = User.objects.get(name=login_name)
+#     if login_password == user.password:
+#         return Response({"status": "ok", "msg": "Log in successfully.", "user_id": user.id},
+#                         status=status.HTTP_200_OK)
+#     else:
+#         return Response({"status": "error", "msg": "Wrong password."},
+#                         status=status.HTTP_400_BAD_REQUEST)
+#     # except api.models.User.DoesNotExist:
+#     #     return Response({"status": "error", "msg": "The user does not exist"},
+#     #                     status=status.HTTP_400_BAD_REQUEST)
+#     # except KeyError:
+#     #     return Response({"status": "error", "msg": "Please check the request body format."},
+#     #                     status=status.HTTP_400_BAD_REQUEST)
+#     # except Exception:
+#     #     return Response({"status": "error", "msg": "Unknown exception."},
+#     #                     status=status.HTTP_400_BAD_REQUEST)
--- galaxeek/settings.py
+++ galaxeek/settings.py
@@ -39,17 +39,52 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api.apps.ApiConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# 支持跨域配置开始
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (
+    'http://1.116.159.212:19192',
+    'http://localhost:114',
+)
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+    'VIEW',
+)
+
+CORS_ALLOW_HEADERS = (
+    'XMLHttpRequest',
+    'X_FILENAME',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'Content-Type',
+)
 
 ROOT_URLCONF = 'galaxeek.urls'
 
